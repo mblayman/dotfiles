8c8e2c9dd45128c7af25a7012786ecfa84331720
Matt Layman <matthewlayman@gmail.com>
2022-12-21 01:40:38 -0500

    Add telescope.

--- nvim/init.lua
+++ nvim/init.lua
@@ -68,6 +68,10 @@ Plug 'numToStr/Comment.nvim'
 -- A fancy status line
 Plug 'nvim-lualine/lualine.nvim'
 
+-- Fuzzy finder
+Plug('nvim-telescope/telescope.nvim', { tag = '0.1.0' })
+Plug('nvim-telescope/telescope-fzf-native.nvim', {['do'] = 'make' })
+
 -- A programming language parser
 --
 -- Tree-sitter is a C library design to do parsing of programming languages.
@@ -373,3 +377,35 @@ require('nvim-treesitter.configs').setup({
 
 -- Enable the quick comment plugin.
 require('Comment').setup()
+
+-- telescope configuration
+require('telescope').setup {
+  defaults = {
+    mappings = {
+      i = {
+        ['<C-u>'] = false,
+        ['<C-d>'] = false,
+      },
+    },
+  },
+}
+
+-- Enable telescope fzf native, if installed
+pcall(require('telescope').load_extension, 'fzf')
+
+-- See `:help telescope.builtin`
+vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
+vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
+vim.keymap.set('n', '<leader>/', function()
+  -- You can pass additional configuration to telescope to change theme, layout, etc.
+  require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+    winblend = 10,
+    previewer = false,
+  })
+end, { desc = '[/] Fuzzily search in current buffer]' })
+
+vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
+vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
+vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
+vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
+vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
