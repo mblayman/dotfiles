37cee0aafb53cb81b991319b66d71528885fe262
Matt Layman <matthewlayman@gmail.com>
2023-01-04 23:54:55 -0500

    Configure lspconfig to set up clangd.

--- nvim/init.lua
+++ nvim/init.lua
@@ -400,6 +400,9 @@ local lspconfig = require('lspconfig')
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
+-- C
+lspconfig.clangd.setup({capabilities = capabilities, on_attach = on_attach})
+
 -- Go
 lspconfig.gopls.setup({capabilities = capabilities, on_attach = on_attach})
 
