be68e2c975bae3003aa5d1445e716dc082d5b4a9
Matt Layman <matthewlayman@gmail.com>
2024-03-16 17:15:50 -0400

    Move gitsigns and config-local.

---
+++ nvim/lua/mblayman/plugins/config_local.lua
@@ -0,0 +1,11 @@
+-- Allow local config files
+--
+-- This feature puts a guard on `exrc` so that Neovim is not unconditionally
+-- sourcing random rc files.
+return {
+  "klen/nvim-config-local",
+  opts = {
+    -- Support the local_vimrc plugin filename that I used to use.
+    config_files = {"_vimrc_local.vim", ".vimrc", ".vimrc.lua"},
+  },
+}
