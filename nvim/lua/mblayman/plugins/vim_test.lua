182f8f990d078ecc36bfb0265ed1ec2302eff2bf
Matt Layman <matthewlayman@gmail.com>
2024-03-16 17:26:27 -0400

    Move vim-test.

---
+++ nvim/lua/mblayman/plugins/vim_test.lua
@@ -0,0 +1,13 @@
+-- Integrate a test runner.
+return {
+  "vim-test/vim-test",
+  config = function()
+    vim.g["test#custom_runners"] = {lua = {"luatest"}}
+
+    -- Run a test file.
+    vim.keymap.set("n", "<leader>f", ":TestFile<cr>", {silent = true})
+
+    -- Run a test.
+    vim.keymap.set("n", "<leader>t", ":TestNearest<cr>", {silent = true})
+  end,
+}
