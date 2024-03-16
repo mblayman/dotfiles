bec1cdc78cbf851028c1254290e0f5f41a6309c2
Matt Layman <matthewlayman@gmail.com>
2024-03-16 15:03:29 -0400

    Move tpope plugins.

--- nvim/lua/mblayman/plugins/init.lua
+++ nvim/lua/mblayman/plugins/init.lua
@@ -1,1 +1,8 @@
-return {}
+return {
+  "tpope/vim-endwise", -- Smart completion of do/end.
+  "tpope/vim-fugitive", -- Git in Vim
+  "tpope/vim-rhubarb", -- Fugitive extensions (for Gbrowse GitHub links)
+  "tpope/vim-sleuth", -- Detect tabstop and shiftwidth automatically
+  "tpope/vim-unimpaired", -- Shortcuts for quickly navigating quicklists and such.
+  "tpope/vim-vinegar", -- Make netrw more awesome.
+}
