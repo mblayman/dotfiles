cb777ceb3c2445b44b542c4653a7aff899c34304
Matt Layman <matthewlayman@gmail.com>
2024-03-16 14:23:14 -0400

    Move ident-blankline to a plugin file.

---
+++ nvim/lua/mblayman/plugins/indent_blankline.lua
@@ -0,0 +1,5 @@
+-- Guidelines UI for indentation
+--
+-- This plugin adds subtle guidelines at indentation stops
+-- so that my eyes can follow the relevant scoping.
+return {"lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {}}
