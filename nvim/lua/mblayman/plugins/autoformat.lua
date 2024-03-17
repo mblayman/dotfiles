c5a59079b74d769364c35cc413c7d8727d4e8ab4
Matt Layman <matthewlayman@gmail.com>
2024-03-17 01:21:15 -0400

    Add autoformat plugin.

---
+++ nvim/lua/mblayman/plugins/autoformat.lua
@@ -0,0 +1,22 @@
+-- Autoformat
+return {
+  "stevearc/conform.nvim",
+  opts = {
+    notify_on_error = true,
+    format_on_save = function(bufnr)
+      -- Disable "format_on_save lsp_fallback" for languages that don't
+      -- have a well standardized coding style. You can add additional
+      -- languages here or re-enable it for the disabled ones.
+      local disable_filetypes = {c = true, cpp = true}
+      return {
+        timeout_ms = 500,
+        lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
+      }
+    end,
+    formatters_by_ft = {
+      -- FIXME: I'm not sure if I want to use stylua, but lua-format is not available.
+      -- lua = {"stylua"},
+      python = {"ruff_fix", "ruff_format"},
+    },
+  },
+}
