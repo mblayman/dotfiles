5e0c5e8de75d828d1d075af420768413b519d36f
Matt Layman <matthewlayman@gmail.com>
2024-06-18 01:26:36 -0400

    Add clang-formatter for C.

--- nvim/lua/mblayman/plugins/autoformat.lua
+++ nvim/lua/mblayman/plugins/autoformat.lua
@@ -7,13 +7,14 @@ return {
       -- Disable "format_on_save lsp_fallback" for languages that don't
       -- have a well standardized coding style. You can add additional
       -- languages here or re-enable it for the disabled ones.
-      local disable_filetypes = { c = true, cpp = true }
+      local disable_filetypes = { cpp = true }
       return {
         timeout_ms = 500,
         lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
       }
     end,
     formatters_by_ft = {
+      c = { "clang-format" },
       lua = { "stylua" },
       python = { "ruff_fix", "ruff_format" },
     },
