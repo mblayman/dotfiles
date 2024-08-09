63e6433acb937532c17356f0f477ea2fbc80bd4c
Matt Layman <matthewlayman@gmail.com>
2024-08-08 21:11:52 -0400

    Add previous plugin config changes.

--- nvim/lua/mblayman/plugins/indent_blankline.lua
+++ nvim/lua/mblayman/plugins/indent_blankline.lua
@@ -2,4 +2,9 @@
 --
 -- This plugin adds subtle guidelines at indentation stops
 -- so that my eyes can follow the relevant scoping.
-return { "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} }
+return {
+  "lukas-reineke/indent-blankline.nvim",
+  main = "ibl",
+  opts = {},
+  enabled = false,
+}
